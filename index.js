e8271a7a5cccdddec681e0e9e1a44bfd8ed8e390
JustinAMB <Justinmb@outlook.es>
2022-03-08 21:55:03 -0600

    cambios generales

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-
 import express from 'express';
 import router from './routes/index.js';
 import db from './config/db.js';
 
 import dotenv from 'dotenv';
-dotenv.config({path:"variables.env"});
+dotenv.config({ path: "variables.env" });
 
-const app=express();
+const app = express();
 db.authenticate()
-    .then(()=>console.log("sirve"))
-    .catch(error=>console.log(error))
+    .then(() => console.log("sirve"))
+    .catch(error => console.log(error))
 
 //habilitar pug
-app.set('view engine','pug');
+app.set('view engine', 'pug');
 //obtener año actual
-app.use((req,res,next)=>{
-    const date=new Date();
-    res.locals.year=date.getFullYear();
-    res.locals.nombresitio="Agencia de Viajes";
+app.use((req, res, next) => {
+    const date = new Date();
+    res.locals.year = date.getFullYear();
+    res.locals.nombresitio = "Agencia de Viajes";
     console.log(res);
     return next();
 });
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //definir la carpeta publica
 app.use(express.static('public'));
-app.use('/',router);
-const port =process.env.PORT || 400;
-const host=process.env.HOST || '0.0.0.0';
-app.listen(port,host,()=>{
+app.use('/', router);
+const port = process.env.PORT || 400;
+const host = process.env.HOST || '0.0.0.0';
+app.listen(port, host, () => {
     console.log(`Funciona ${port}     ${host}`);
 })
